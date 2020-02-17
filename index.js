c105ad564722d66a07edc4c1cde198d26f7d5a0b
Blair MacIntyre <bmacintyre@mozilla.com>
2020-02-17 10:18:33 -0500

    basic redirect

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express = require('express')
 const app = express()
-app.get('/', (req, res) => {
-  res.send('HEY!')
-})
+
+const targetBaseUrl = 'http://ieeevr.org/2020/';
+
+function handleRedirect(req, res) {
+  const targetUrl = targetBaseUrl + req.originalUrl;
+  res.redirect(targetUrl);
+}
+
+app.get('*', handleRedirect);
+
+//app.get('/', (req, res) => {
+//  res.send('HEY!')
+//})
 
 app.listen(3000, () => console.log('Server running on port 3000'))
