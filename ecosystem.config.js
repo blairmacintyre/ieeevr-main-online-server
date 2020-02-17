1d74ca7c01fc5bed33f6b5c0cf523f3dcb2dcb9e
Blair MacIntyre <bmacintyre@mozilla.com>
2020-02-17 09:54:19 -0500

    Setup PM2

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [{
+    name: 'ieeevr-server',
+    script: './index.js'
+  }],
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: 'ec2-54-86-218-127.compute-1.amazonaws.com',
+      key: '~/.ssh/ieeevrawstoplevelserversshkey.pem',
+      ref: 'origin/master',
+      repo: 'git@github.com:blairmacintyre/ieeevr-main-online-server.git',
+      path: '/home/ubuntu/ieeevr-server',
+      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+    }
+  }
+}
